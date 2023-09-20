0d4daf42257c52e6cdaee62057ebc5f64f5a93c4
FreddyCruger <FreddyCruger@users.noreply.github.com>
2023-09-20 05:40:33 +0400

    Update record_playlist_generator.ps1
    remove unused code

--- record_playlist_generator.ps1
+++ record_playlist_generator.ps1
@@ -1,5 +1,4 @@
 Invoke-WebRequest -Uri 'https://www.radiorecord.ru/api/stations' -ContentType "application/json" -Method Get -UseBasicParsing -OutFile data.json
 $json = Get-Content -Raw -Path "$PSScriptRoot\data.json" | ConvertFrom-Json
-$nums = $json.result.stations
 $playlists = $json.result.stations.stream_320
 $playlists.foreach({$json.result.stations.stream_320 | Out-File $PSScriptRoot\RadioRecord.m3u})
