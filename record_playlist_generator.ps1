b44055de8353c978d9131b4345b898796a2e73f2
FreddyCruger <FreddyCruger@users.noreply.github.com>
2025-05-10 12:38:27 +0400

    Update record_playlist_generator.ps1
    Update script

--- record_playlist_generator.ps1
+++ record_playlist_generator.ps1
@@ -1,4 +1,11 @@
-Invoke-WebRequest -Uri 'https://www.radiorecord.ru/api/stations' -ContentType "application/json" -Method Get -UseBasicParsing -OutFile data.json
-$json = Get-Content -Raw -Path "$PSScriptRoot\data.json" | ConvertFrom-Json
+# Получаем данные с API и сохраняем их в переменную
+$response = Invoke-WebRequest -Uri 'https://www.radiorecord.ru/api/stations' -ContentType "application/json" -Method Get -UseBasicParsing
+
+# Преобразуем содержимое ответа из JSON
+$json = $response.Content | ConvertFrom-Json
+
+# Извлекаем плейлисты
 $playlists = $json.result.stations.stream_320
-$playlists.foreach({$json.result.stations.stream_320 | Out-File $PSScriptRoot\RadioRecord.m3u})
+
+# Записываем плейлисты в файл
+$playlists | Out-File -FilePath "$PSScriptRoot\RadioRecord.m3u" -Encoding UTF8
